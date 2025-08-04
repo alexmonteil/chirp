93629c2e93ee6d16b7a8664fa787c9d2c269856d
alexmonteil <alex.monteil@outlook.com>
2025-08-03 21:32:16 -0500

    Added zod schema for validation on comment entity

---
+++ src/validation/comment.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+// Zod schema for comment
+export const commentSchema = z.object({
+  body: z.string().min(1, { message: "comment cannot be empty" }),
+  chirpId: z
+    .number()
+    .int()
+    .positive({ message: "chirpId must be a positive integer" }),
+  authorId: z
+    .number()
+    .int()
+    .positive({ message: "authorId must be a positive integer" }),
+});
