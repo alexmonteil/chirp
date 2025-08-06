f9673acf8edbd5cbfe3e52c8816325b571df8b3f
alexmonteil <alex.monteil@outlook.com>
2025-08-06 00:47:26 -0500

    finished register and login handlers

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,14 @@
 import { Hono } from "hono";
 import type { AuthEnv } from "../types/authEnv.js";
 import { zValidator } from "@hono/zod-validator";
+import { sign } from "hono/jwt";
 import { registerSchema } from "../validation/register.js";
 import { loginSchema } from "../validation/login.js";
 import bcrypt from "bcrypt";
 import { saltRoundsSchema } from "../validation/saltRounds.js";
 import { safeParse } from "../utils/utils.js";
+import { credentials, users } from "../db/schema.js";
+import { eq } from "drizzle-orm";
 
 const authRouter = new Hono<AuthEnv>();
 
@@ -25,9 +28,58 @@ const HASH_SALT_ROUNDS = safeParse<number>(
 // POST /register
 authRouter.post("/register", zValidator("json", registerSchema), async (c) => {
   const { username, email, password } = c.req.valid("json");
+  const passwordHash = await bcrypt.hash(password, HASH_SALT_ROUNDS);
+  const db = c.get("db");
+
+  const result = await db.transaction(async (tx) => {
+    // create the user record
+    const newUser = await tx
+      .insert(users)
+      .values({ username, email })
+      .returning();
+
+    // create the credentials record
+    await tx
+      .insert(credentials)
+      .values({ userId: newUser[0].id, passwordHash });
+    return newUser[0];
+  });
+
+  return c.json(result, 201);
 });
 
 // POST /login
-authRouter.post("/login", zValidator("json", loginSchema), async (c) => {});
+authRouter.post("/login", zValidator("json", loginSchema), async (c) => {
+  const { email, password } = c.req.valid("json");
+  const db = c.get("db");
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, email),
+    with: {
+      credentials: true,
+    },
+  });
+
+  if (!user || !user.credentials) {
+    return c.json({ message: "Invalid email or password" }, 401);
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    password,
+    user.credentials.passwordHash
+  );
+
+  if (!isPasswordValid) {
+    return c.json({ message: "Invalid email or password" }, 401);
+  }
+
+  const jwtPayload = {
+    sub: user.id,
+    email: user.email,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60,
+  };
+
+  const token = await sign(jwtPayload, JWT_SECRET);
+  return c.json({ token });
+});
 
 export default authRouter;
