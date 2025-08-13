431027d7b03a48363014e53edb7796e56bb53e98
alexmonteil <alex.monteil@outlook.com>
2025-08-13 14:49:47 -0500

    Added configuration to enable chakra-ui components being used in the react app

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Flex } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <Flex direction="row" align="center" justify="center" gap={4}>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </Flex>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
