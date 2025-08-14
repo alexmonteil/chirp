bdf0e05c2117ccb96804a856bdac55bfb539b41e
alexmonteil <alex.monteil@outlook.com>
2025-08-13 19:24:24 -0500

    Created UI skeleton for register form

--- client/src/features/auth/components/RegisterForm.tsx
+++ client/src/features/auth/components/RegisterForm.tsx
@@ -1,12 +1,63 @@
-import { VStack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Field,
+  Fieldset,
+  Input,
+  VStack,
+  Text,
+  Link,
+  Stack,
+} from "@chakra-ui/react";
 
 const RegisterForm = () => {
   return (
-    <VStack p={8} borderWidth={1} borderRadius="lg" boxShadow="lg" bg="white">
-      <Text fontSize="2xl" fontWeight="bold" color="black">
-        Register
-      </Text>
-    </VStack>
+    <Fieldset.Root size="lg" maxW="lg" minW="md">
+      <VStack p={8}>
+        <Fieldset.Legend
+          fontFamily="mono"
+          fontSize="2xl"
+          color="white"
+          fontWeight="semibold"
+          margin={2}
+        >
+          Create your account
+        </Fieldset.Legend>
+        <Fieldset.Content>
+          <Field.Root required>
+            <Field.Label>
+              Username <Field.RequiredIndicator />
+            </Field.Label>
+            <Input placeholder="username" name="username" type="text" />
+            <Field.ErrorText>Invalid username</Field.ErrorText>
+          </Field.Root>
+          <Field.Root required>
+            <Field.Label>
+              Email address <Field.RequiredIndicator />
+            </Field.Label>
+            <Input placeholder="email" name="email" type="email" />
+            <Field.ErrorText>Invalid email</Field.ErrorText>
+          </Field.Root>
+          <Field.Root required>
+            <Field.Label>
+              Password <Field.RequiredIndicator />
+            </Field.Label>
+            <Input placeholder="password" name="password" type="password" />
+            <Field.ErrorText>Invalid password</Field.ErrorText>
+          </Field.Root>
+        </Fieldset.Content>
+        <Button type="submit" w="100%" mt={3} bgColor="#443691">
+          Register
+        </Button>
+        <Stack direction="row" h="20" w="100%">
+          <Text fontSize="sm" mt={2} fontFamily="mono">
+            Already have an account ?
+          </Text>
+          <Link href="#" display="inline" mt={2} fontFamily="mono">
+            Sign in
+          </Link>
+        </Stack>
+      </VStack>
+    </Fieldset.Root>
   );
 };
 
