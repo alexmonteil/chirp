3130beb6e67dc72534567b14135a2a989be94df8
alexmonteil <alex.monteil@outlook.com>
2025-08-19 21:16:46 -0500

    DEBUGGING SHARED TYPES NOT WORKING WITH DOCKER STAGED BUILD

--- client/src/features/auth/components/RegisterForm.tsx
+++ client/src/features/auth/components/RegisterForm.tsx
@@ -3,13 +3,30 @@ import {
   Field,
   Fieldset,
   Input,
-  VStack,
-  Text,
   Link,
   Stack,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "@chirp/shared";
+import { useForm } from "react-hook-form";
+import type z from "zod";
+
+type RegisterFormValues = z.infer<typeof registerSchema>;
+
+const registerUser = async (values: RegisterFormValues) => {
+  console.log(values);
+};
 
 const RegisterForm = () => {
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormValues>({
+    resolver: zodResolver(registerSchema),
+  });
+
   return (
     <Fieldset.Root size="lg" maxW="lg" minW="md">
       <VStack p={8}>
@@ -19,30 +36,46 @@ const RegisterForm = () => {
           color="white"
           fontWeight="semibold"
           margin={2}
+          onSubmit={handleSubmit(registerUser)}
         >
           Create your account
         </Fieldset.Legend>
         <Fieldset.Content>
-          <Field.Root required>
+          {/* Username field */}
+          <Field.Root required invalid={!!errors.username}>
             <Field.Label>
               Username <Field.RequiredIndicator />
             </Field.Label>
             <Input placeholder="username" name="username" type="text" />
             <Field.ErrorText>Invalid username</Field.ErrorText>
           </Field.Root>
-          <Field.Root required>
+          {/* Email field */}
+          <Field.Root required invalid={!!errors.email}>
             <Field.Label>
               Email address <Field.RequiredIndicator />
             </Field.Label>
             <Input placeholder="email" name="email" type="email" />
             <Field.ErrorText>Invalid email</Field.ErrorText>
           </Field.Root>
-          <Field.Root required>
+          {/* Password field */}
+          <Field.Root required invalid={!!errors.password}>
             <Field.Label>
               Password <Field.RequiredIndicator />
             </Field.Label>
             <Input placeholder="password" name="password" type="password" />
             <Field.ErrorText>Invalid password</Field.ErrorText>
+          </Field.Root>
+          {/* Password confirmation field */}
+          <Field.Root required invalid={!!errors.passwordConfirm}>
+            <Field.Label>
+              Confirm Password <Field.RequiredIndicator />
+            </Field.Label>
+            <Input
+              placeholder="confirm password"
+              name="passwordConfirm"
+              type="password"
+            />
+            <Field.ErrorText>Passwords do not match</Field.ErrorText>
           </Field.Root>
         </Fieldset.Content>
         <Button type="submit" w="100%" mt={3} bgColor="#443691">
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://server:3000",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  //   fs: {
+  //     allow: ["../packages"],
+  //   },
+  // },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //     "@shared": path.resolve(__dirname, "../packages/"),
+  //   },
+  // },
 });
